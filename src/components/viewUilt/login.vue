b1fc1302242f25d15200a48700d5ccd7ac972a17
LJYnotbaybay <1303251680@qq.com>
2018-10-16 10:10:32 +0800

    a vue porjct

---
+++ src/components/viewUilt/login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login">
+        <div class="banner">
+            <div> <img src="http://localhost:8080/static/login_banner/ChMkJ1brrCqIfo7DABzwBFnFOXQAAN8GgMvcKsAHPAc524.png" alt=""></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'login'
+    }
+</script>
+
+<style>
+
+</style>
+
